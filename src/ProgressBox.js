85b7a0c09c0b094dcf36d654e4f86c5140ca3c07
alext <alextselegidis@gmail.com>
2018-03-19 11:33:58 +0100

    Corrected index value of progress box component.

--- src/ProgressBox.js
+++ src/ProgressBox.js
@@ -3,9 +3,11 @@ import './ProgressBox.css';
 
 class ProgressBox extends Component {
     render() {
+        const index = !this.props.state.question ? this.props.state.currentQuestionIndex : this.props.state.currentQuestionIndex + 1;
+
         return (
             <div className="ProgressBox">
-                <h1>Question {this.props.state.currentQuestionIndex} of {this.props.state.config.questions.length}</h1>
+                <h1>Question {index} of {this.props.state.config.questions.length}</h1>
             </div>
         )
     }
